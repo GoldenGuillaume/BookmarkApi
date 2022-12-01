7dce4b64b660bab032b78e35f3213236c0d7ee77
Guillaume Doré <contact@guillaumedore.com>
2022-12-01 11:50:50 +0100

    add get endpoint, configure swagger

---
+++ src/GoldenGuillaume.BookmarkApi.Web/Models/BookmarkTreeNode.cs
@@ -0,0 +1,37 @@
+﻿namespace GoldenGuillaume.BookmarkApi.Web.Models
+{
+    public class BookmarkTreeNode
+    {
+        public string Id { get; set; } = null!;
+
+        public BookmarkTreeNode[]? Children { get; set; }
+
+        public long? DateAdded { get; set; }
+
+        public long? DateGroupModified { get; set; }
+
+        public int? Index { get; set; }
+
+        public string? ParentId { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string? Url { get; set; }
+
+        public int CountChildrenElements()
+        {
+            int counter = 0;
+            CountChildrenNodes(this, ref counter);
+            return counter;
+        }
+
+        private void CountChildrenNodes(BookmarkTreeNode node, ref int count)
+        {
+            foreach (BookmarkTreeNode child in node.Children ?? Enumerable.Empty<BookmarkTreeNode>())
+            {
+                count++;
+                CountChildrenNodes(child, ref count);
+            }
+        }
+    }
+}
--- src/GoldenGuillaume.BookmarkApi.Web/Program.cs
+++ src/GoldenGuillaume.BookmarkApi.Web/Program.cs
@@ -1,9 +1,16 @@
+using System.Reflection;
+using System.Text.Json;
+using GoldenGuillaume.BookmarkApi.Web.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"));
+});
 
 var app = builder.Build();
 
@@ -11,33 +18,49 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+var bookmarksGroup = app.MapGroup("api/bookmarks").WithOpenApi();
+
+bookmarksGroup.MapGet("/", GetBookmarks);
+bookmarksGroup.MapPost("/", SynchronizeBookmarks);
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+async Task<IResult> GetBookmarks(IWebHostEnvironment environment)
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    var savedBookmarks = await LoadSavedBookmarks(environment);
+    return savedBookmarks != null ? TypedResults.Ok(savedBookmarks) : TypedResults.Ok();
+}
+
+async Task<IResult> SynchronizeBookmarks(HttpContext context, IWebHostEnvironment environment, BookmarkTreeNode browserBookmarks)
+{
+    var savedBookmarks = await LoadSavedBookmarks(environment);
+    if (savedBookmarks != null && JsonSerializer.Serialize(savedBookmarks) != JsonSerializer.Serialize(browserBookmarks))
+    {
+
+    }
+
+    string filePath = $"{environment.WebRootPath}{Path.DirectorySeparatorChar}{builder.Configuration.GetValue<string>("BookmarksFileName")}";
+    using FileStream stream = File.Create(filePath);
+    await JsonSerializer.SerializeAsync(stream, browserBookmarks, new JsonSerializerOptions { WriteIndented = true });
+    await stream.DisposeAsync();
+
+    return savedBookmarks == null ? TypedResults.Created(context.Request.Path) : TypedResults.Ok(browserBookmarks);
+}
+
+async Task<BookmarkTreeNode?> LoadSavedBookmarks(IWebHostEnvironment environment)
+{
+    if (File.Exists($"{environment.WebRootPath}{Path.DirectorySeparatorChar}{builder.Configuration.GetValue<string>("BookmarksFileName")}"))
+    {
+        using FileStream stream = File.OpenRead($"{environment.WebRootPath}{Path.DirectorySeparatorChar}{builder.Configuration.GetValue<string>("BookmarksFileName")}");
+        return await JsonSerializer.DeserializeAsync<BookmarkTreeNode>(stream);
+    }
+    return await Task.FromResult<BookmarkTreeNode?>(null);
 }
